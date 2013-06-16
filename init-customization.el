d4a07b92191db05511d67530c43bfac90bb0914b
Brian Coleman <brianfcoleman@gmail.com>
2013-06-16 16:30:17 +0100

    emacs-config - Customization change to trust the Monokai theme.

--- init-customization.el
+++ init-customization.el
@@ -6,7 +6,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "4dacec7215677e4a258e4529fac06e0231f7cdd54e981d013d0d0ae0af63b0c8" "36a309985a0f9ed1a0c3a69625802f87dee940767c9e200b89cdebdb737e5b29" default)))
+ '(custom-safe-themes (quote ("3c708b84612872e720796ea1b069cf3c8b3e909a2e1da04131f40e307605b7f9" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "4dacec7215677e4a258e4529fac06e0231f7cdd54e981d013d0d0ae0af63b0c8" "36a309985a0f9ed1a0c3a69625802f87dee940767c9e200b89cdebdb737e5b29" default)))
  '(ecb-options-version "2.40")
  '(ede-project-directories (quote nil)))
 (custom-set-faces
