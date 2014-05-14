187fb11670380412c2023565542a052aa61b95a9
Brian Coleman <brianfcoleman@gmail.com>
2014-05-15 00:59:15 +0100

    emacs-config - disable fci-mode as it can cause emacs to appear to hang.

--- init.el
+++ init.el
@@ -301,9 +301,15 @@ before visiting a new tags table"
   (setq fill-column 80)
 
   (require 'fill-column-indicator)
-  (when bc-dark-background-p
-    (setq fci-rule-color "#ffffff"))
-  (fci-mode 1))
+; Disable fci mode as it sometimes causes emacs to appear to hang by not
+; redisplaying. I can reproduce this by opening js2-mode.el and positioning the
+; cursor at the line defining (defconst js2-side-effecting-tokens. Then jump
+; to the end of the definition using end-of-defun. emacs appears to hang as
+; the buffer it is not redisplayed.
+; (when bc-dark-background-p
+;   (setq fci-rule-color "#ffffff"))
+; (fci-mode 1)
+)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Setup saving
