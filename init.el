a167d56210967079eb4349eeeffd87cd3a84c9fd
Brian Coleman <brianfcoleman@gmail.com>
2014-05-16 00:13:33 +0100

    emacs-config - change the font size on osx.

--- init.el
+++ init.el
@@ -346,7 +346,7 @@ before visiting a new tags table"
 
 (defun bc-setup-fonts ()
   (cond ((eq system-type 'darwin)
-         (set-frame-font "Menlo-13" nil t))
+         (set-frame-font "Menlo-14" nil t))
         ((eq system-type 'gnu/linux)
          (set-frame-font "Liberation Mono-11" nil t))
         ((eq system-type 'windows-nt)
